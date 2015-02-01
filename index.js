933ebdde0c23e618c5a44ac7a936a04c5f8cf563
jonschlinkert <jon.schlinkert@sellside.com>
2015-02-01 10:41:17 -0500

    first pass

--- index.js
+++ index.js
@@ -16,5 +16,50 @@ module.exports = function (str, options) {
   if (str.slice(-1) === '.') {
     return str.slice(0, str.length - 1);
   }
+  str = fixLead(str, 'MIT');
   return str;
 };
+
+function fixLead(str, type) {
+  str = str.replace(/\r/g, '');
+  var lines = str.split('\n');
+  var regex = testLead(type);
+  var first = lines[0];
+
+  if (regex.test(first)) {
+    lines[0] = expectedLead(type);
+    str = lines.join('\n');
+  } else {
+    str = expectedLead(type) + '\n\n' + str;
+  }
+
+  return str;
+}
+
+/**
+ * TODO: add more types
+ */
+
+function testLead(type) {
+  var regex = '';
+
+  switch(type) {
+    case 'MIT':
+      regex = /The[ \t]MIT[ \t]License[ \t]\(MIT\)/i;
+      break;
+  }
+
+  return regex;
+}
+
+function expectedLead(type) {
+  var lead = '';
+
+  switch(type) {
+    case 'MIT':
+      lead = 'The MIT License (MIT)';
+      break;
+  }
+
+  return lead;
+}
